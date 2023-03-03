5074bf350a623d4cdb9fe9301504e998fbf352b3
xieshangyu <76835319+xieshangyu@users.noreply.github.com>
2023-03-02 22:55:53 -0500

    level1,and level2
    level1,and level2

---
+++ Assets/code/trap.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class trap : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public string levelToLoad;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+
+
+}
