7ca4c506183d1974c7fd1e23789bade9ce5e4cd5
Yilin Zhao <zy2280@nyu.edu>
2023-03-10 10:50:18 -0500

    .

--- Assets/code/player.cs
+++ Assets/code/player.cs
@@ -39,7 +39,8 @@ public class player : MonoBehaviour
         }
         if(publicvar.life<=0)
         {
-            SceneManager.LoadScene(levelToLoad);
+            //SceneManager.LoadScene(levelToLoad);
+            SceneManager.LoadScene("FailPage");
         }
 
     }
